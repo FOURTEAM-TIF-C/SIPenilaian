2d02d0f6252202a99de54bb788465914eb92d47f
Ramadhanty-PC\Ramadhanty <dhanty12@gmail.com>
2018-04-24 11:40:28 +0700

    master

---
+++ SIPenilaian/app/src/main/java/com/example/ramadhanty/sipenilaian/NilaiSiswa.java
@@ -0,0 +1,13 @@
+package com.example.ramadhanty.sipenilaian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NilaiSiswa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nilai_siswa);
+    }
+}
